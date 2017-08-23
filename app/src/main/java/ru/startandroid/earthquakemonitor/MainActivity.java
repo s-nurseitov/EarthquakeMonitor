bb11a12af8e9f2cff938d4650e22a908c0439877
Serik <serik_siti@mail.ru>
2017-08-24 02:33:08 +0600

    Initial commit

---
+++ app/src/main/java/ru/startandroid/earthquakemonitor/MainActivity.java
@@ -0,0 +1,104 @@
+package ru.startandroid.earthquakemonitor;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Elements elements;
+    static public ArrayList<String> titleList=new ArrayList<String>();
+    static public ArrayList<LatLng> latLngs=new ArrayList<>();
+    static public int position;
+    static public ArrayList<String> magnitude=new ArrayList<>();
+    static public ArrayList<String> depth=new ArrayList<>();
+    static public ArrayList<String> time=new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter = null;
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView=(ListView)findViewById(R.id.listView);
+
+        Thread thread=new Thread();
+        thread.execute();
+
+
+        arrayAdapter = new ArrayAdapter<String>(this,
+                R.layout.list_item,R.id.pro_item, titleList);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                MainActivity.position = position;
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public class Thread extends AsyncTask<String,Void, String>{
+
+        @Override
+        protected String doInBackground(String... arg) {
+
+            try {
+                Document document = Jsoup.connect("http://zeml.info/online/").get();
+
+                elements= document.select(".table-place");
+
+                for(Element element: elements){
+                    titleList.add(element.text());
+                }
+
+                Element table = document.select("tbody").first();
+                Elements rows = table.select("tr");
+
+                for (int i = 0; i < rows.size(); i++) {
+                    Element row = rows.get(i);
+                    Elements cols = row.select("td");
+                    for(int j=1;j<cols.size();j+=4){
+                        Element colTime = cols.get(j);
+                        j++;
+                        Element colLatitude=cols.get(j);
+                        j++;
+                        Element colLongitude=cols.get(j);
+                        LatLng latLng=new LatLng(Double.parseDouble(colLatitude.text().toString()),
+                                Double.parseDouble(colLongitude.text().toString()));
+                        time.add(colTime.text().toString());
+                        latLngs.add(latLng);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            listView.setAdapter(arrayAdapter);
+        }
+    }
+}
